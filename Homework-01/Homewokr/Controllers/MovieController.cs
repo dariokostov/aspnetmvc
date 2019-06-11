ad7e603630f5fe642a4d15940964b487b9332436
Dario Kostov <kostov.dario@gmail.com>
2019-06-11 19:04:15 +0200

    uploadhm1

---
+++ Homework-01/Homewokr/Controllers/MovieController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Homewokr.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homewokr.Controllers
+{
+    public class MovieController : Controller
+    {
+        public static List<MovieModel> MoviesList = new List<MovieModel>
+        {
+            new MovieModel{
+                Id = 1,
+                Name = "Interstellar",
+                Genre = "SciFi",
+                Rating = 8.8,
+                Length = 124
+            },
+            new MovieModel{
+                Id = 2,
+                Name = "The Upside",
+                Genre = "Drama",
+                Rating = 6.7,
+                Length = 110
+            },
+            new MovieModel{
+                Id = 3,
+                Name = "Guardians of the galaxy",
+                Genre = "SciFi",
+                Rating = 7.9,
+                Length = 113
+            },
+            new MovieModel{
+                Id = 4,
+                Name = "Up",
+                Genre = "Animation",
+                Rating = 8.7,
+                Length = 129
+            },
+
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Movies()
+        {
+            return View(MoviesList);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult MovieDetails(MovieModel model)
+        {
+            var movie = MoviesList.FirstOrDefault(m => m.Id == model.Id);
+            return View(movie);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(MovieModel movie)
+        {
+            MoviesList.Add(movie);
+            return View("Movies", MoviesList);
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            var movie = MoviesList.FirstOrDefault(m => m.Id == id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateMovie(MovieModel movie)
+        {
+            var movieIndex = MoviesList.FindIndex(m => m.Id == movie.Id);
+            MoviesList[movieIndex] = movie;
+            return View("Movies", MoviesList);
+        }
+
+    }
+}
